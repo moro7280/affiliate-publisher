2f782077df63113b4f5029147ab59f9d5c7c9df5
moro7280 <pascucci.moreno3@gmail.com>
2026-02-16 22:10:21 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'oaidalleapiprodscus.blob.core.windows.net' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
